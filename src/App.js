f5ecc14e0599c426603a8f94754451b336fc8eff
Jonathan Eccles <dev@ecclesagile.com>
2013-09-18 09:26:21 -0400

    Filter out blocked records and block the first one returned

--- src/App.js
+++ src/App.js
@@ -6,12 +6,22 @@ Ext.define('CustomApp', {
     },
     fireFromTheStore:function(store,records){
         var blockedRecords = _.filter(records,function(record){
-            var blocked = record.get('Blocked')
+            var blocked = record.get('Blocked');
             console.log(blocked,record);
             return blocked;
         });
-        console.log(this);
-        debugger;
+    
+        var record = records[0];
+        record.set("Blocked", true);
+        record.save({
+            callback: function(result, operation) {
+                if(operation.wasSuccessful()) {
+                    //Get the new defect's objectId
+                    var objectId = result.get('ObjectID');
+                    console.log(result.get('Name')+" oid:" +objectId+" blocked and saved");
+                }
+            }
+        });
         Ext.Msg.alert('Status', 'Store Loaded with '+records.length+' records and '+blockedRecords.length + ' blocked records.');
     },
 
@@ -24,10 +34,16 @@ Ext.define('CustomApp', {
                     console.log(data);
                 }
             },
+            filters: [{
+                property: 'Blocked',
+                value:false
+            }],
+            
             autoLoad:true,
             fetch: ['Name', 'ScheduleState', 'Blocked']
         });
         
         store.on("load", this.fireFromTheStore, this);
     }
+    
 });
